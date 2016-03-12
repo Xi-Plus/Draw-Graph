e5a448955a069f7f81641b4449376dcc1f71900f
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-12 18:12:45 +0800

    Ignore Multiple Edges

--- draw-graph.js
+++ draw-graph.js
@@ -4,14 +4,22 @@ function submit() {
 	// set URL Parameters
 	var directed = $("#directed").prop('checked')?'1':'0';
 	var ignore = $("#ignore").prop('checked')?'1':'0';
+	var ignore_mul_edge = $("#ignore_mul_edge").prop('checked')?'1':'0';
 	var data = $("#data").val();
-	var url = location.pathname+"?"+"data="+encodeURIComponent(data)+"&directed="+directed+"&ignore="+ignore;
 	window.history.replaceState("","",url);
+	var url = location.pathname+"?"+"data="+encodeURIComponent(data)+"&directed="+directed+"&ignore="+ignore+"&ignore_mul_edge="+ignore_mul_edge;
+}
+
+function check_edge(multiple_edges,start,end){
+	if (multiple_edges[start][end] !== undefined) return 2;
+	if (multiple_edges[end][start] !== undefined) return 1;
+	return 0;
 }
 
 function draw_graph() {
 	var directed = $("#directed").prop('checked');
 	var ignore = $("#ignore").prop('checked');
+	var ignore_mul_edge = $("#ignore_mul_edge").prop('checked');
 
 	var data = $("#data").val();
 	data = data.split('\n');
@@ -34,21 +42,30 @@ function draw_graph() {
 		nodes_set.add(data[i][1]);
 	}
 	var nodes = [];
+	var multiple_edges = [];
 	for (var node of nodes_set) {
 		nodes.push({id: node, label: node});
+		multiple_edges[node] = [];
 	}
 
 	// create edges
 	var edges = [];
 	for (var i in data) {
 		if (data[i].length < 2) continue;
+		if (ignore_mul_edge) {
+			if (directed && check_edge(multiple_edges,data[i][0],data[i][1]) >= 2) continue;
+			else if (!directed && check_edge(multiple_edges,data[i][0],data[i][1]) >= 1) continue;
+		}
 		var edge = {from: data[i][0], to: data[i][1]};
 		if (data[i].length >= 3) {
 			edge.label = data[i][2];
 		}
 		if (directed) {
 			edge.arrows = 'to';
+		} else {
+			multiple_edges[data[i][1]][data[i][0]] = true;
 		}
+		multiple_edges[data[i][0]][data[i][1]] = true;
 		edges.push(edge);
 	}
 
